bfae886dfe32659063b08f2e9c41599059fb0ddd
RestuAdityaSetyawan <112872721+RestuAdityaSetyawan@users.noreply.github.com>
2022-09-06 10:46:43 +0700

    Add files via upload

---
+++ Tugas1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama: Restu Aditya Setyawan \nNim: A11.2022.14654 \nAsal Sekolah: Smk N 2 Kendal" << endl;
+    return 0;
+}
